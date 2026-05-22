ed0f4c3cb412ffd4564c0a038599c38110b5ac09
Ibrahim Oyeniyi <ibrahimoyeniyi21@gmail.com>
2026-05-22 23:58:38 +0100

    Improved landing page design.

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,68 +1,39 @@
 import Nav from "../nav/nav";
 import './home.css';
+import objectives from '../data,.json';
+import { Link } from 'react-router-dom';
 
 function Home(){
     return(
         <div className="home">
-            <header className="home_header">
+            <header className="home_header" style={{
+                height: '100vh',
+                background: 'linear-gradient(rgba(15, 76, 129, 0.7), rgba(15, 76, 129, 0.7)), url(/header.jpg)',
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'left center'
+            }}>
                 <Nav />
+                <div className="info">
+                    <h1 className="brand-title">Kindness For The Less Priviledged</h1>
+                    <p>
+                        Healthcare outreach and humanitarian support for underserved communities.
+                    </p>
+
+                    <button type="button"><Link to="/contact">Contact Us</Link></button>
+                </div>
             </header>
             <main className="home_main">
-                <div className="info_style" style={{
-                backgroundImage: 'url(/images.jpeg)',
-                backgroundSize: 'cover',
-                backgroundRepeat: 'no-repeat'
-                }}>
-                    <p className="info">
-                        The Kindness for the Less Privildege initiative is a voluntary humanitarian intervention championed by Chief Innocent Atuogu Agu Eji Maranba 1 of Akwakuma Autonomous Community.
-                    </p>
-                    <div className="image_container">
-
-                    </div>
-                </div>
                 <section className="about" id="about">
-                    <h2>Objectives</h2>
+                    <h2>Our Objectives</h2>
                     <div className="objs">
-                        <article className="article one">
-                            <img src="/med-supplies.jpg" alt="Medical Equipments" />
-                            <p>
-                                Provide medical supplies and equipments to healthcare centers.
-                            </p>
-                        </article>
-
-                        <article className="article two">
-                            <img src="/spec.jpg" alt="Health Screening" />
-                            <p>
-                                Offer health education and screening to marginalized groups.
-                            </p>
-                        </article>
-
-                        <article className="article three">
-                            <img src="/health-edu.jpg" alt="Doctor treating a patient in a bed" />
-                            <p>
-                                Collaborate with healthcare professionals to deliver specialized care.
-                            </p>
-                        </article>
-
-                        <article className="article four">
-                            <img src="/cont-health.jpg" alt="Researcher conducting a medical experiment" />
-                            <p>
-                                Establish sustainable healthcare programs for long term impact.
-                            </p>
-                        </article>
-
-                        <article className="article five">
-                            <img src="/food-supplies.jpg" alt="People delivering supplies to a community" />
-                            <p>
-                                Distribute essential supplies e.g food, water, etc...
-                            </p>
-                        </article>
-                        <article className="article six">
-                            <img src="/header-bg.jpg" alt="People holding hand for support" />
-                            <p>
-                                Provide psychological support and counselling.
-                            </p>
-                        </article>
+                        {objectives.map((item, index) => {
+                            return <article className="card" key={index}>
+                                <img src={item.image} alt={item.title} className="obj-img" />
+                                <h3 className="card-title">{item.title}</h3>
+                                <p className="card-info">{item.text}</p>
+                            </article>
+                        })}
                     </div>
                 </section>
                 <section className="people">
