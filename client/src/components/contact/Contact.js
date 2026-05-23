d5ca2470295e77a7d5aa69fd575be7e6ab65fcbb
Ibrahim Oyeniyi <ibrahimoyeniyi21@gmail.com>
2026-05-23 15:05:52 +0100

    Initialized contact page.

--- client/src/components/contact/Contact.js
+++ client/src/components/contact/Contact.js
@@ -1,4 +1,18 @@
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+const yupSchema = yup.object({
+    name: yup.string().required(),
+    email: yup.string().email().required(),
+    message: yup.string().max(1000).required()
+})
+
 function Contact(){
+    const { handleSubmit, formState: { errors }, register } = useForm({
+        resolver: yupResolver(yupSchema)
+    });
+
     return(
         <div className="contact">
             
