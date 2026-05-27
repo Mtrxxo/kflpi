fa994d877449320ff996c4050812bf524457baab
Ibrahim Oyeniyi <ibrahimoyeniyi21@gmail.com>
2026-05-27 09:07:04 +0100

    Implemented router.

--- server/router/contactRouter.js
+++ server/router/contactRouter.js
@@ -2,7 +2,11 @@ const express = require('express');
 const xssSanitize = require('xss-sanitize');
 const schema = require('../schema/schema');
 const validate = require('../middleware/validate');
+const rateLimiter = require('../middleware/rateLimit');
+const logic = require('../controller/contactController');
 
 const router = express.Router();
 
-router.post('/contact', )
+router.post('/contact', rateLimiter, xssSanitize.paramSanitize(), validate.validateInput(schema), logic);
+
+module.exports = router;
