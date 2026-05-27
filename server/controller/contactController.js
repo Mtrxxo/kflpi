4f0ee2e02410c98f6815ae33df6b5035664370ae
Ibrahim Oyeniyi <ibrahimoyeniyi21@gmail.com>
2026-05-27 08:57:34 +0100

    Implemented logic controller.

---
+++ server/controller/contactController.js
@@ -0,0 +1,17 @@
+const message = require('../model/contactModel');
+
+const sendMessage = async(req, res) => {
+    try{
+        const data = req.validatedBody;
+
+        await message.create({
+            ...data
+        });
+
+        res.json({message: "Successfully sent message."})
+    }catch(err){
+        res.json({message: 'Message not sent. Try sending it later.'})
+    }
+}
+
+module.exports = sendMessage
